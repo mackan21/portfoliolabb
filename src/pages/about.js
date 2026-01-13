37895c4a5991bae896033304b54fdedae23a5be4
Marcus <mackan166@hotmail.com>
2026-01-13 21:21:22 +0100

    About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,45 @@
 import * as React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
 import Layout from "../components/Layout";
+import * as styles from "../styles/about.module.css";
 
 const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query AboutPageQuery {
+      contentfulAboutpage(slug: { eq: "about" }) {
+        title
+        description {
+          description
+        }
+        image {
+          gatsbyImageData(width: 1200, placeholder: BLURRED)
+        }
+      }
+    }
+  `);
+
+  const about = data.contentfulAboutpage;
+  const image = getImage(about.image);
+
   return (
     <Layout>
-      <h1>About</h1>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>{about.title}</h1>
+
+        {image && (
+          <div className={styles.imageWrap}>
+            <GatsbyImage
+              image={image}
+              alt={about.title}
+              className={styles.image}
+            />
+          </div>
+        )}
+
+        <p className={styles.text}>{about.description?.description}</p>
+      </div>
     </Layout>
   );
 };
